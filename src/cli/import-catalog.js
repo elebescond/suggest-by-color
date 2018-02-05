5bac231b48ef7b63cf9bb446107cb69081355b71
Erwan LE BESCOND <elebescond@gmail.com>
2018-02-05 23:50:25 +0100

    Improve import-catalog command

---
+++ src/cli/import-catalog.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import validUrl from 'valid-url';
+import { BAD_REQUEST_CATALOG } from '../core/const';
+import { importCatalog } from '../core/import-catalog';
+
+var argv = require('yargs')
+  .usage('Usage: $0 -catalog [url]')
+  .demandOption(['catalog'])
+  .argv;
+
+// TODO Validate url input
+importCatalog(argv.catalog)
+  .then(products => {  
+    products.forEach(product => {
+      console.log(`${product.id} imported`)
+    });
+  })
+  .catch(console.log)
--- src/core/import-catalog.js
+++ src/core/import-catalog.js
@@ -3,22 +3,38 @@ import productDao from './dao/product'
 import request from 'request'
 import transform from 'stream-transform'
 
+const mapCsvToDatastore = product => {
+  return {
+    id: product.id,
+    photo: 'https:' + product.photo,
+    url: product.url,
+    color: null
+  }
+}
+
 const extractFromCsv = catalogUrl => {
   
   var promise = new Promise((resolve, reject) => {
     let products = []
+    
     let options = {
       delimiter: ';'
     }
+    
     let stream = request.get(catalogUrl)
+      .on('error', err => reject(err))
+    
     csv(options)
       .fromStream(stream)
       .on('json', product => {
-        products.push(product)
+        products.push(mapCsvToDatastore(product))
       })
       .on('done', err => {
         if (err) {
            return reject(err)
+        }
+        if(products.length == 0) {
+          return reject(new Error('No product detected'))
         }
         resolve(products)
       })
@@ -28,19 +44,20 @@ const extractFromCsv = catalogUrl => {
 }
 
 const saveProducts = products => {
-  const limit = 10;
+  const limit = 3
   return Promise.all(products.reduce((promises, product, index) => {
     // What chain do we add it to?
-    const chainNum = index % limit;
-    let chain = promises[chainNum];
+    const chainNum = index % limit
+    let chain = promises[index]
+
     if (!chain) {
         // New chain
-        chain = promises[chainNum] = Promise.resolve();
+        chain = promises[index] = Promise.resolve(product)
     }
     // Add it
-    promises[chainNum] = chain.then(_ => productDao.save(product));
-    return promises;
-  }, []));
+    promises[index] = chain.then(_ => productDao.save(product))
+    return promises
+  }, []))
 }
 
 /**
